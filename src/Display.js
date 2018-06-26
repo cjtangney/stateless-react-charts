852cc512dc26a5d6a895c290640dcb1eaa44c78a
Connor Tangney <Couch3ater@gmail.com>
2018-06-26 14:59:15 -0400

    parse out airline and airport claims data

--- src/Display.js
+++ src/Display.js
@@ -1,12 +1,233 @@
 import React from 'react';
 import { AirlineChart, AirportChart } from './Charts';
 
-const Display = (c) => {
+const Display = (e) => {
 	const getChart = (chart) => {
-		if (chart === 'airline') {
-			return <AirlineChart data={[11, 75, 35, 63, 77, 78, 48, 89, 9, 97, 69, 7]}/>;
-		} else {
-			return <AirportChart data={[53, 64, 97, 33, 37, 30, 4, 15, 13, 37, 15, 79]}/>;
+		if(e.data !== undefined){
+			let csvData = e.data;
+			var year, month, claimAmnt;
+		  if (chart === 'airline') {
+			  /*
+			      the Airline object contains all the claims that belong to a particular airline
+			  */
+			  class Airline {
+			    constructor(name, claims, yr2010, yr2011, yr2012, yr2013){
+			      this.name = name;
+			      this.claims = [claims];
+			      this.yr2010 = [];
+			      this.yr2011 = [];
+			      this.yr2012 = [];
+			      this.yr2013 = [];
+			    }
+			  }
+			  class MonthlyClaim {
+			    constructor(month, value, sum){
+			      this.month = month;
+			      this.value = [value];
+			      this.sum = sum;
+			    }
+
+			    updateSum(val){
+			      this.sum += val;
+			    }
+			  }
+			  var airlineClaims = [];
+			  /*
+			    iterate through csv data;
+			    if output data structure doesn't contain airline, create new Airline obj, push claim data;
+			    if output data structure contains current airline, locate it, push claim data;
+			  */
+			  csvData.forEach(function(val){
+			    if(!(airlineClaims.some(function(element){ return element.name === val["Airline Name"].trim() }))){
+			      airlineClaims.push(new Airline(val["Airline Name"].trim(), val));
+			    }else{
+			      airlineClaims[airlineClaims.findIndex(item => item.name === val["Airline Name"].trim())].claims.push(val);
+			    }
+			  });
+			  /*  
+			      iterate through airlineClaims;
+			      iterate through each airline's claim data;
+			      determine year;
+			      determine month;
+			      if output data structure doesn't contain month, create new MonthlyClaim obj, push value
+			      if output data structure contains current month, locate it, push value
+			  */
+			  airlineClaims.forEach(function(airline){
+			    airline.claims.forEach(function(claim){
+			      year = claim["Date Received"].split("-")[2];
+			      month = claim["Date Received"].split("-")[1];
+			      claimAmnt = Number(claim["Close Amount"].replace(/[^0-9\.-]+/g,""))
+			      switch(year){
+			        case "10" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airline.yr2010.some(function(element){ return element.month === month }))){
+			              airline.yr2010.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airline.yr2010[airline.yr2010.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airline.yr2010[airline.yr2010.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "11" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airline.yr2011.some(function(element){ return element.month === month }))){
+			              airline.yr2011.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airline.yr2011[airline.yr2011.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airline.yr2011[airline.yr2011.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "12" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airline.yr2012.some(function(element){ return element.month === month }))){
+			              airline.yr2012.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airline.yr2012[airline.yr2012.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airline.yr2012[airline.yr2012.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "13" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airline.yr2013.some(function(element){ return element.month === month }))){
+			              airline.yr2013.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airline.yr2013[airline.yr2013.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airline.yr2013[airline.yr2013.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        default :
+			          break;
+			      }
+			    });
+			  });
+			  /*
+			      set airlines with "-" to name "Unknown";
+			      remove the last row from the data (<BR>);
+			  */
+			  airlineClaims[airlineClaims.findIndex(item => item.name === "-")].name = "Unknown";
+			  airlineClaims.splice(-1, airlineClaims.length-1);
+			  console.log(airlineClaims);
+				return <AirlineChart data={[11, 75, 35, 63, 77, 78, 48, 89, 9, 97, 69, 7]}/>;
+			} else {
+			  /*
+			      the Airport object contains all the claims that belong to a particular airport
+			  */
+			  class Airport {
+			    constructor(name, claims, yr2010, yr2011, yr2012, yr2013){
+			      this.name = name;
+			      this.claims = [claims];
+			      this.yr2010 = [];
+			      this.yr2011 = [];
+			      this.yr2012 = [];
+			      this.yr2013 = [];
+			    }
+			  }
+			  class MonthlyClaim {
+			    constructor(month, value, sum, avg){
+			      this.month = month;
+			      this.value = [value];
+			      this.sum = sum;
+			      this.avg = 0;
+			    }
+			    updateSum(val){
+			      this.sum += val;
+			    }
+			    calcAvg(){
+			      this.avg = this.sum / this.value.length;
+			      /*  
+			          ?? stringified version of average for display purposes??
+			          this.avg = (this.sum / this.value.length).toFixed(2);
+			      */
+			    }
+			  }
+			  var airportClaims = [];
+			  /*
+			      iterate through csv data;
+			      if output data structure doesn't contain airport, create new Airport obj, push claim data;
+			      if output data structure contains current airport, locate it, push claim data;
+			  */
+			  csvData.forEach(function(val){
+			    if(!(airportClaims.some(function(element){ return element.name === val["Airport Code"].trim() }))){
+			      airportClaims.push(new Airport(val["Airport Code"].trim(), val));
+			    }else{
+			      airportClaims[airportClaims.findIndex(item => item.name === val["Airport Code"].trim())].claims.push(val);
+			    }
+			  });
+			  /*  
+			      iterate through airportClaims;
+			      iterate through each airport's claim data;
+			      determine year;
+			      determine month;
+			      if output data structure doesn't contain month, create new MonthlyClaim obj, push value
+			      if output data structure contains current month, locate it, push value
+			  */
+			  airportClaims.forEach(function(airport){
+			    airport.claims.forEach(function(claim){
+			      year = claim["Date Received"].split("-")[2];
+			      month = claim["Date Received"].split("-")[1];
+			      claimAmnt = Number(claim["Close Amount"].replace(/[^0-9\.-]+/g,""))
+			      switch(year){
+			        case "10" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airport.yr2010.some(function(element){ return element.month === month }))){
+			              airport.yr2010.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airport.yr2010[airport.yr2010.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airport.yr2010[airport.yr2010.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "11" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airport.yr2011.some(function(element){ return element.month === month }))){
+			              airport.yr2011.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airport.yr2011[airport.yr2011.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airport.yr2011[airport.yr2011.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "12" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airport.yr2012.some(function(element){ return element.month === month }))){
+			              airport.yr2012.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airport.yr2012[airport.yr2012.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airport.yr2012[airport.yr2012.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        case "13" :
+			          if(claimAmnt !== 0 && !(isNaN(claimAmnt))){
+			            if(!(airport.yr2013.some(function(element){ return element.month === month }))){
+			              airport.yr2013.push(new MonthlyClaim(month, claimAmnt, claimAmnt));
+			            }else{
+			              airport.yr2013[airport.yr2013.findIndex(item => item.month === month)].value.push(claimAmnt);
+			              airport.yr2013[airport.yr2013.findIndex(item => item.month === month)].updateSum(claimAmnt);
+			            }
+			          }
+			          break;
+			        default :
+			          break;
+			      }
+			    });
+			    //  update average here
+			    airport.yr2010.forEach(function(month){ month.calcAvg(); });
+			    airport.yr2011.forEach(function(month){ month.calcAvg(); });
+			    airport.yr2012.forEach(function(month){ month.calcAvg(); });
+			    airport.yr2013.forEach(function(month){ month.calcAvg(); });
+			  });
+			  /*
+			      set airports with "-" to name "Unknown";
+			      remove the last row from the data (<BR>);
+			  */
+			  airportClaims[airportClaims.findIndex(item => item.name === "-")].name = "Unknown";
+			  airportClaims.splice(-1, airportClaims.length-1);
+				return <AirportChart data={[53, 64, 97, 33, 37, 30, 4, 15, 13, 37, 15, 79]}/>;
+			}
 		}
 	}
 
@@ -15,7 +236,7 @@ const Display = (c) => {
 			<div className='level'>
 				<div className='level-item'>
 					<div className='conatiner is-fluid has-text-centered'>
-						{getChart(c.chart)}
+						{getChart(e.chart)}
 					</div>
 				</div>
 			</div>
@@ -34,13 +255,13 @@ const Display = (c) => {
 	);
 }
 
-const Tabs = (t) => {
+const Tabs = (e) => {
 	return (
 		<section id='tabs'>
 			<div className='container is-fluid'>
 				<div className='tabs is-centered is-fullwidth'>
 				  <ul>
-				  	{t.tabs.map(function callback(tab, i) {
+				  	{e.tabs.map(function callback(tab, i) {
 			  			if(i===0) { return(<li key={'tab' + tab} className='is-active'><a>{tab}</a></li>); } else { return(<li key={'tab' + tab}><a>{tab}</a></li>); }
 						})} 
 				  </ul>
--- src/index.js
+++ src/index.js
@@ -9,33 +9,33 @@ import { Tabs, Display } from './Display';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-		this.updateData = this.updateData.bind(this);
-	}
+		this.state = {
+			data: this.props.data	
+		}
 
-	componentDidMount() {
-		let csvFile = require("./data/claims-2010-2013_0.csv");
+		//initialize workers to parse data
+    //let airlineWorker = new Worker('./workers/AirlineWorker.js');
+		let csvFile = require('' + this.props.file + '');
 	  Papa.parse(csvFile, {
 	    header: true,
 	    download: true,
 	    skipEmptyLines: true,
-	    complete: this.updateData
+	    complete: (result) => {
+	    	const data = result.data;
+	    	this.setState({ data });
+	    }
 		});
-	}
-
-	updateData(result) {
-		const data = result.data;
-		this.setState({ data });
 	}
 
 	render() {
 		return (
 	    <div>
 	      <Tabs tabs={['2010', '2011', '2012', '2013']}/>
-	      <Display chart='airline' /*data={this.state.data}*//>
-	      <Display chart='airport' /*data={this.state.data}*//>
+	      <Display chart='airline' data={this.state.data} />
+	      <Display chart='airport' data={this.state.data} />
 	    </div>
 	  );
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App file='./data/claims-2010-2013_0.csv' />, document.getElementById('root'));
